0c2367eb0a6c6c1722ef2556837e4ff7a670306c
Cosmin <grigore.cosmin20@yahoo.com>
2022-04-10 22:25:24 +0300

    unittest + comments

--- tema/marketplace.py
+++ tema/marketplace.py
@@ -35,9 +35,9 @@ class Marketplace:
         :param queue_size_per_producer: the maximum size of a queue associated with each producer
         """
         # key = producer_id, value = nr products in queue
-        self.producers_size = {}
-        # list of lists [id_producer, product]
-        self.products = []
+        self.producers_queue_len = {}
+        # list of lists [id_producer, product] - available products
+        self.stock_products = []
 
         # key - id_cart, val - list of tuples [id_producer, product]
         self.list_of_carts = {}
@@ -49,7 +49,7 @@ class Marketplace:
         # Consumers locks
         self.cart_id = Lock()
         self.place_order_lock = Lock()
-        self.products_lock = Lock()
+        self.stock_products_lock = Lock()
 
 
 
@@ -58,11 +58,12 @@ class Marketplace:
         """
         Returns an id for the producer that calls this.
         """
+        # returnez nr-ul cumparatorului
         with self.producers_id:
             self.logger.info("Starting  register_producer")
-            self.producers_size[len(self.producers_size)] = 0
+            self.producers_queue_len[len(self.producers_queue_len)] = 0
             self.logger.info("Finished  register_producer")
-        return len(self.producers_size) - 1
+        return len(self.producers_queue_len) - 1
 
     def publish(self, producer_id, product):
         """
@@ -79,10 +80,10 @@ class Marketplace:
 
 
         self.logger.info("Starting  publish product %s by prod id %d", product, producer_id)
-        if self.producers_size[producer_id] < self.queue_size_per_producer:
-            self.producers_size[producer_id] += 1
-            self.products.append([producer_id, product])
-
+        if self.producers_queue_len[producer_id] < self.queue_size_per_producer:
+            # adaug produsul in lista de produse si incrementez nr de produse publicate.
+            self.producers_queue_len[producer_id] += 1
+            self.stock_products.append([producer_id, product])
             self.logger.info("Finished  publish product %s by prod id %d", product, producer_id)
             return True
 
@@ -97,6 +98,7 @@ class Marketplace:
         """
         with self.cart_id:
             self.logger.info("Starting  new_cart")
+            # creez un nou cart
             self.list_of_carts[len(self.list_of_carts)] = []
             self.logger.info("Finished  new_cart")
 
@@ -114,18 +116,17 @@ class Marketplace:
 
         :returns True or False. If the caller receives False, it should wait and then try again
         """
-        # Daca produsul e disponibil il adauga in cart si returneaza True,
-        # altfel returneaza False
 
-        with self.products_lock:
-            self.logger.info("Starting  add_to_cart")
-            for prod in self.products:
+        with self.stock_products_lock:
+            self.logger.info("Starting  add %s to cart with id %d", product, cart_id)
+            for prod in self.stock_products:
                 if prod[1] == product:
-                    self.products.remove(prod)
+                    # Daca am gasit produsul in stock il scot din stoc si il adaug in cos
+                    self.stock_products.remove(prod)
                     self.list_of_carts[cart_id].append([prod[0], product])
                     self.logger.info("Finished  add_to_cart")
                     return True
-            self.logger.info("Finished  add_to_cart")
+            self.logger.info("Finished  add %s to cart with id %d", product, cart_id)
         return False
 
     def remove_from_cart(self, cart_id, product):
@@ -138,13 +139,14 @@ class Marketplace:
         :type product: Product
         :param product: the product to remove from cart
         """
-        self.logger.info("Starting  remove_from_cart")
+        self.logger.info("Starting  remove %s from cart with id %d ", product, cart_id)
         for produs in self.list_of_carts[cart_id]:
             if product == produs[1]:
-                self.products.append(produs)
+                #  Am gasit produsul in cos, il adaug inapoi in stock si il scot din cos
+                self.stock_products.append(produs)
                 self.list_of_carts[cart_id].remove(produs)
                 break
-        self.logger.info("Finished remove_from_cart")
+        self.logger.info("Finished  remove %s from cart with id %d ", product, cart_id)
 
 
     def place_order(self, cart_id):
@@ -156,82 +158,95 @@ class Marketplace:
         """
         cart_list = []
         with self.place_order_lock:
-            self.logger.info("Starting place_order")
+            self.logger.info("Starting place order for cart with id %d ", cart_id)
             for prod in self.list_of_carts[cart_id]:
+                #  Fac lista cu produsele din cos, le scad din coada producatorilor si printez
                 print(currentThread().name, "bought", prod[1])
                 cart_list.append(prod[1])
-                self.producers_size[prod[0]] -= 1
-            self.logger.info("Finished place_order")
+                self.producers_queue_len[prod[0]] -= 1
+            self.logger.info("Finished place order for cart with id %d ", cart_id)
         return cart_list
 
 class TestMarketplace(unittest.TestCase):
+    """  Unit Tests  """
+
     def setUp(self):
         """Initializations"""
         self.marketplace = Marketplace(2)
-        self.products = [Tea("Tei", 10, "Herbal"), Coffee(name='Cappuccino', price=2, acidity=4.02, roast_level='MEDIUM')]
+        self.stock_products = [Tea("Tei", 10, "Herbal"),
+        Coffee(name='Cappuccino', price=2, acidity=4.02, roast_level='MEDIUM')]
         self.carts = [ [ {
                         "type": "add",
-                        "product": self.products[0],
+                        "product": self.stock_products[0],
                         "quantity": 1
                     },
                     {
                         "type": "add",
-                        "product": self.products[1],
+                        "product": self.stock_products[1],
                         "quantity": 3
                     } ] ]
 
     def test_register_producer(self):
-        """ Test register_producer """
+        """ Register 2 producers """
         self.assertEqual(self.marketplace.register_producer(), 0, "register FAILED")
         self.assertEqual(self.marketplace.register_producer(), 1, "register FAILED")
 
     def test_publish(self):
-        """ Test publish """
+        """ Publish 3 products with queue size = 2 """
         prod_id = self.marketplace.register_producer()
-        self.assertTrue(self.marketplace.publish(prod_id, self.products[1]), "publish FAILED")
-        self.assertTrue(self.marketplace.publish(prod_id, self.products[0]), "publish FAILED")
-        self.assertFalse(self.marketplace.publish(prod_id, self.products[0]), "publish FAILED")
+        self.assertTrue(self.marketplace.publish(prod_id, self.stock_products[1]), "publish FAIL")
+        self.assertTrue(self.marketplace.publish(prod_id, self.stock_products[0]), "publish FAIL")
+        self.assertFalse(self.marketplace.publish(prod_id, self.stock_products[0]), "publish FAIL")
 
 
     def test_new_cart(self):
-        """ Test new cart """
+        """ Register new cart """
         self.assertEqual(self.marketplace.new_cart(), 0, "new_cart FAILED")
         self.assertEqual(self.marketplace.new_cart(), 1, "new_cart FAILED")
 
     def test_add_to_cart(self):
-        """ Test add_to_cart """
+        """
+        Publish 2 products, register a cart, one producer and add only 2 products
+        """
         prod_id = self.marketplace.register_producer()
-        self.marketplace.publish(prod_id,self.products[1])
-        self.marketplace.publish(prod_id,self.products[0])
+        self.marketplace.publish(prod_id,self.stock_products[1])
+        self.marketplace.publish(prod_id,self.stock_products[0])
         cart_id = self.marketplace.new_cart()
-        self.assertTrue( self.marketplace.add_to_cart(cart_id, self.products[1]), "add_to_cart FAILED")
-        self.assertTrue(self.marketplace.add_to_cart(cart_id, self.products[0]), "add_to_cart FAILED")
-        self.assertFalse(self.marketplace.add_to_cart(cart_id, self.products[0]), "add_to_cart FAILED")
+        first_add = self.marketplace.add_to_cart(cart_id, self.stock_products[1])
+        self.assertTrue(first_add, "add_to_cart FAILED")
+        second_add = self.marketplace.add_to_cart(cart_id, self.stock_products[0])
+        self.assertTrue(second_add, "add_to_cart FAILED")
+        third_add = self.marketplace.add_to_cart(cart_id, self.stock_products[0])
+        self.assertFalse(third_add, "add_to_cart FAILED")
 
     def test_remove_from_cart(self):
-        """ Test remove from cart """
+        """
+        Publish 2 products, add 2 products to cart, remove one and another one.
+        """
         prod_id = self.marketplace.register_producer()
         cart_id = self.marketplace.new_cart()
-        self.marketplace.publish(prod_id,self.products[1])
-        self.marketplace.publish(prod_id,self.products[0])
-        self.marketplace.add_to_cart(cart_id, self.products[0])
-        self.marketplace.add_to_cart(cart_id, self.products[1])
-        self.marketplace.remove_from_cart(cart_id, self.products[0])
+        self.marketplace.publish(prod_id,self.stock_products[1])
+        self.marketplace.publish(prod_id,self.stock_products[0])
+        self.marketplace.add_to_cart(cart_id, self.stock_products[0])
+        self.marketplace.add_to_cart(cart_id, self.stock_products[1])
+        self.marketplace.remove_from_cart(cart_id, self.stock_products[0])
         self.assertEqual(len(self.marketplace.list_of_carts[0]), 1,"remove_from_cart FAILED")
-        self.marketplace.remove_from_cart(cart_id, self.products[0])
+        self.marketplace.remove_from_cart(cart_id, self.stock_products[0])
         self.assertEqual(len(self.marketplace.list_of_carts[0]), 1,"remove_from_cart FAILED")
 
     def test_place_order(self):
-        """ Test place order """
+        """
+        Place an order with 2 producs.
+        """
         prod_id = self.marketplace.register_producer()
         cart_id = self.marketplace.new_cart()
-        self.marketplace.publish(prod_id,self.products[1])
-        self.marketplace.publish(prod_id,self.products[0])
-        self.marketplace.add_to_cart(cart_id, self.products[0])
-        self.marketplace.add_to_cart(cart_id, self.products[1])
-        self.marketplace.remove_from_cart(cart_id, self.products[0])
-        self.marketplace.add_to_cart(cart_id, self.products[0])
-        products_ref = [self.products[1], self.products[0]]
+        self.marketplace.publish(prod_id,self.stock_products[1])
+        self.marketplace.publish(prod_id,self.stock_products[0])
+        self.marketplace.add_to_cart(cart_id, self.stock_products[0])
+        self.marketplace.add_to_cart(cart_id, self.stock_products[1])
+        self.marketplace.remove_from_cart(cart_id, self.stock_products[0])
+        self.marketplace.add_to_cart(cart_id, self.stock_products[0])
+        products_ref = [self.stock_products[1], self.stock_products[0]]
         products_out = self.marketplace.place_order(cart_id)
-        for i in range(len(products_ref)):
-            self.assertIs(products_out[i], products_ref[i], "place_order FAILED")
+        for prod in products_out:
+            self.assertIn(prod, products_ref, "place_order FAILED")
